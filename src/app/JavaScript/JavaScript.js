46d41640814f67d9f28e842d7e3ccd5e69d6cd71
Vamsi Kankipati <vamsibh1@gmail.com>
2019-10-20 11:51:05 +0530

    useStyles hooks added

--- src/app/JavaScript/JavaScript.js
+++ src/app/JavaScript/JavaScript.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { createUseStyles } from 'react-jss'
 
 export default function JavaScript() {
+
+    const useStyles = createUseStyles({
+        style: {
+            color: 'green',
+            fontWeight: 'bold',
+            marginLeft: '1rem'
+        }
+    });
+
+    const classes = useStyles();
+
     return (
-        <div>
+        <div className={classes.style}>
             <p>{'JavaScript ES'}</p>
         </div>
     );
